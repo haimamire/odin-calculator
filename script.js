28246ccf43e0a70aced063b42bb997158a2fe521
haimamire <82563520+haimamire@users.noreply.github.com>
2025-06-13 19:13:07 +0900

    Make it display everything in real time

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-const displayDiv = document.querySelector(".display");
-
-const numberButtons = document.querySelectorAll(".number");
-
-// Operator selectors
-const operatorButtons = document.querySelectorAll(".operator");
-
-const equalButton = document.querySelector("#equal-button");
-
 let currentOperation = {
     firstNumber: "",
     operator: "",
     secondNumber: ""
 }
+
+const displayDiv = document.querySelector(".display");
+
+// Math buttons selectors
+const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
+const equalButton = document.querySelector("#equal-button");
+
 
 // Basic operations
 function sum(a, b) {
@@ -35,31 +34,37 @@ function isOperationEmpty() {
     return false;
 }
 
-function displayResult(displayContent) {
-    displayDiv.textContent = displayContent;
+function display(displayContent) {
+    displayDiv.textContent += displayContent;
+}
+function clearDisplay() {
+    displayDiv.textContent = "";
 }
 
+// Event listeners
 numberButtons.forEach(button => {
     button.addEventListener(
         "click",
         (e) => {
             if (currentOperation.firstNumber === "" || currentOperation.operator === "") {
                 currentOperation.firstNumber += e.target.id;
+
             } else {
                 currentOperation.secondNumber += e.target.id;
             }
+            display(e.target.id);
         }
     )
 });
 
-
-// Save first number and operator when an operator button is pressed
 operatorButtons.forEach(button => {
     button.addEventListener(
         "click",
         (e) => {
             if (currentOperation.firstNumber !== "" && currentOperation.operator === "") {
                 currentOperation.operator = e.target.textContent;
+
+                display(e.target.textContent);
             }
         }
     )
@@ -72,35 +77,24 @@ equalButton.addEventListener(
             let firstNumber = currentOperation.firstNumber;
             let operator = currentOperation.operator;
             let secondNumber = currentOperation.secondNumber;
+            let result;
 
             switch(operator) {
                 case "+":
-                    displayResult(
-                        sum(firstNumber, secondNumber)
-                    );
+                    result = sum(firstNumber, secondNumber)
                     break;
                 case "-":
-                    displayResult(
-                        substract(firstNumber, secondNumber)
-                    );
+                    result = substract(firstNumber, secondNumber);
                     break;
                 case "*":
-                    displayResult(
-                        multiply(firstNumber, secondNumber)
-                    );
+                    result = multiply(firstNumber, secondNumber);
                     break;
                 case "/":
-                    displayResult(
-                        divide(firstNumber, secondNumber)
-                    );
+                    result = divide(firstNumber, secondNumber);
                     break;
             }
-
+            clearDisplay();
+            display(result);
         }
     }
 )
-    // If empty return message and clear everything
-// When = button is pressed, save second number and call operate function
-    // If empty return just first number
-// Switch case and call respective operation functions
-// Return value, call displayResult function
