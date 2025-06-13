60c4efacc846533f05cbab854881a11f684dae7c
haimamire <82563520+haimamire@users.noreply.github.com>
2025-06-13 12:20:23 +0900

    Write structure and buttons to HTML

--- script.js
+++ script.js
@@ -12,4 +12,18 @@ function divide(a, b) {
     return Math.round((a / b) * 100) / 100;
 }
 
+let userOperation = {
+    firstNumber: 0,
+    operator: "",
+    secondNumber: 0
+}
+
 // console.log(divide(10, 3));
+
+// Save first number and operator when an operator button is pressed
+    // If empty return message and clear everything
+// When = button is pressed, save second number and call operate function
+    // If empty return just first number
+// Switch case and call respective operation functions
+// Display result
+
