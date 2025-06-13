07852109c3f97cec8e344c3f482e49fe13eb3634
haimamire <82563520+haimamire@users.noreply.github.com>
2025-06-13 17:10:34 +0900

    Create basic listener that saves current operation

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 const displayDiv = document.querySelector(".display");
 
-// Numbers
-const button1 = document.querySelector("#one");
+// Number selectors
+const oneButton = document.querySelector("#one");
 
-// Operators
+// Operator selectors
 const sumButton = document.querySelector("#sum-button")
 
 const equalButton = document.querySelector("#equal-button");
 
 let currentOperation = {
     firstNumber: undefined,
-    operator: "",
+    operator: undefined,
     secondNumber: undefined
 }
 
+// Basic operations
 function sum(a, b) {
     return a + b;
 }
@@ -32,13 +33,24 @@ function displayResult(displayContent) {
     displayDiv.textContent = displayContent;
 }
 
+oneButton.addEventListener(
+    "click",
+    (e) => {
+        if (currentOperation.firstNumber === undefined) {
+            currentOperation.firstNumber = 1;
+        } else {
+            currentOperation.secondNumber = 1;
+        }
+        console.table(currentOperation);
+    }
+)
 
 // Save first number and operator when an operator button is pressed
 sumButton.addEventListener(
     "click",
     (e) => {
         if (currentOperation.firstNumber === undefined) {
-            alert("undefined");
+
         } else {
             currentOperation.operator = "+";
         }
